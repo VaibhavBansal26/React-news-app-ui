d7d8b71146e236e39ed06cf062e9f957585aca71
Vaibhav Bansal <vaibhav.bansal945@gmail.com>
2020-07-27 03:02:59 +0530

    ALL done

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header = () => (
+    <header>
+        <div>Daily News</div>
+    </header>
+)
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux';
+import {createStore,applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import reducers from './store/reducers/index';
+
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware)(createStore)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
